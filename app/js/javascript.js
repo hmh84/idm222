34aec3e302bd03fa43969923dbae27f52dfec6c0
Hunter <hmh84@drexel.edu>
2020-02-06 18:40:01 -0500

    Revert

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,6 +1,6 @@
 document.querySelector('.toggleButton').addEventListener('click', toggleSideBar);
 
 function toggleSideBar() {
-    var element = document.getElementById("nav");
+    var element = document.getElementById("sideBar");
     element.classList.toggle("sideBar_active");
 }
