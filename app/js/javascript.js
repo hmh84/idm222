dd7a3f7d3ab2382ca918d705c75c2d8353a4b381
Hunter <hmh84@drexel.edu>
2020-02-06 18:38:18 -0500

    Reverting :rotating_light:
    -CSS issues undefined.

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,3 +1,5 @@
+document.querySelector('.toggleButton').addEventListener('click', toggleSideBar);
+
 function toggleSideBar() {
     var element = document.getElementById("nav");
     element.classList.toggle("sideBar_active");
