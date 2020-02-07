96bf7bc9bd780d93e0ed95de87935cf881873d4f
Hunter <hmh84@drexel.edu>
2020-02-06 20:03:02 -0500

    Revert "Revert"
    This reverts commit 34aec3e302bd03fa43969923dbae27f52dfec6c0.

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,6 +1,6 @@
 document.querySelector('.toggleButton').addEventListener('click', toggleSideBar);
 
 function toggleSideBar() {
-    var element = document.getElementById("sideBar");
+    var element = document.getElementById("nav");
     element.classList.toggle("sideBar_active");
 }
