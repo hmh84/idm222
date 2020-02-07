23f7a5878699441344e5e87301b28d159cfb58e9
Hunter <hmh84@drexel.edu>
2020-02-06 20:03:04 -0500

    Revert "Reverting :rotating_light:"
    This reverts commit dd7a3f7d3ab2382ca918d705c75c2d8353a4b381.

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,5 +1,3 @@
-document.querySelector('.toggleButton').addEventListener('click', toggleSideBar);
-
 function toggleSideBar() {
     var element = document.getElementById("nav");
     element.classList.toggle("sideBar_active");
