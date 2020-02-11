a9c72dfe6bd24a8b8b1eac2827b156e80652773e
Hunter <hmh84@drexel.edu>
2020-02-11 11:44:17 -0500

    Almost there :clock11:
    -Fixed up JS
    -Created div containing the sign you selected
    -Setup function to set contents of the signOutput div (WIP)

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,6 +1,15 @@
 document.querySelector('#toggleButton').addEventListener('click', toggleNav);
 
 function toggleNav() {
-    var element = document.querySelector(".nav");
-    element.classList.toggle("navActive");
+    var element = document.querySelector('.nav');
+    element.classList.toggle('navActive');
+    document.querySelector('.navBack').removeAttribute('hidden');
+}
+
+document.querySelector('.navBack').addEventListener('click', navBackHidden);
+
+function navBackHidden() {
+    toggleNav();
+    document.querySelector('.navBack').hidden = true;
+
 }
